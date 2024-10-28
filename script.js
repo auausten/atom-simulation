65a07e41c6f727be1d3bb3418e9c76348ee98dba
auausten <133758570+auausten@users.noreply.github.com>
2024-10-28 18:37:52 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,137 @@
-// variable for current state of simulation
-let state = {
-    protons: 1, // num of protons
-    neutrons: 0, // num of neutrons
-    electrons: 2, // num of electrons
+import * as THREE from 'three';
+import * as dat from 'dat.gui';
+
+const ATOMS =
+{   'Hydrogen':
+    {   atomicNum: 1,
+        atomicMass: 1,
+        atomicSymbol: 'H',
+    },
+    'Helium':
+    {   atomicNum: 2,
+        atomicMass: 4,
+        atomicSymbol: 'He',
+    },
+    'Lithium':
+    {   atomicNum: 3,
+        atomicMass: 7,
+        atomicSymbol: 'Li',
+    },
+    'Beryllium':
+    {   atomicNum: 4,
+        atomicMass: 9,
+        atomicSymbol: 'Be',
+    },
+    'Boron':
+    {   atomicNum: 5,
+        atomicMass: 11,
+        atomicSymbol: 'B',
+    },
+    'Carbon':
+    {   atomicNum: 6,
+        atomicMass: 12,
+        atomicSymbol: 'C',
+    },
+    'Nitrogen':
+    {   atomicNum: 7,
+        atomicMass: 14,
+        atomicSymbol: 'N',
+    },
+    'Oxygen':
+    {   atomicNum: 8,
+        atomicMass: 16,
+        atomicSymbol: 'O',
+    },
+    'Fluorine':
+    {   atomicNum: 9,
+        atomicMass: 19,
+        atomicSymbol: 'F',
+    },
+    'Neon':
+    {   atomicNum: 10,
+        atomicMass: 20,
+        atomicSymbol: 'Ne',
+    },
+    'Sodium':
+    {   atomicNum: 11,
+        atomicMass: 23,
+        atomicSymbol: 'Na',
+    },
+    'Magnesium':
+    {   atomicNum: 12,
+        atomicMass: 24,
+        atomicSymbol: 'Mg',
+    },
+    'Aluminum':
+    {   atomicNum: 13,
+        atomicMass: 27,
+        atomicSymbol: 'Al',
+    },
+    'Silicon':
+    {   atomicNum: 14,
+        atomicMass: 28,
+        atomicSymbol: 'Si',
+    },
+    'Phosphorus':
+    {   atomicNum: 15,
+        atomicMass: 31,
+        atomicSymbol: 'P',
+    },
+    'Sulfur':
+    {   atomicNum: 16,
+        atomicMass: 32,
+        atomicSymbol: 'S',
+    },
+    'Chlorine':
+    {   atomicNum: 17,
+        atomicMass: 35,
+        atomicSymbol: 'Cl',
+    },
+    'Argon':
+    {   atomicNum: 18,
+        atomicMass: 40,
+        atomicSymbol: 'Ar',
+    },
+    'Potassium':
+    {   atomicNum: 19,
+        atomicMass: 39,
+        atomicSymbol: 'K',
+    },
+    'Calcium':
+    {   atomicNum: 20,
+        atomicMass: 40,
+        atomicSymbol: 'Ca',
+    }
 };
 
-massNumber = state.protons + state.neutrons;
-ionicCharge = state.protons - state.electrons;
+const scene = new THREE.Scene(window.innerWidth, window.innerHeight);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-if (ionicCharge > 0) {
-    console.log("Cation")
-    console.log("Ionic Charge: " + ionicCharge)
-} else if (ionicCharge < 0) {
-    console.log("Anion")
-    console.log("Ionic Charge: " + ionicCharge)
-} else {
-    console.log("No Ionic Charge")
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const radius = 0.5;
+const widthSegments = 32;
+const heightSegments = 32;
+
+const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+const material = new THREE.MeshBasicMaterial({color: 0xffffff});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+camera.position.z = 3;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
 }
+
+animate();
+
+const gui = new dat.GUI();
+dropdown = gui.addFolder('Atom');
